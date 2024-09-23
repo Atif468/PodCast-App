65ce13bfa5920dced376e50a958b0905b7721625
Mohd Atif Ansari <114794817+Atif468@users.noreply.github.com>
2024-09-23 16:34:03 +0530

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,19 @@
 import Aboutus from "../components/Aboutus";
 import Welcome from "../components/Welcome";
 import Footer from "../components/Footer";
-
+import { CardSpotlight } from "../components/ui/card-spotlight";
 function LandingPage() {
   return (
     <>
+    <div className="">
+     <CardSpotlight className="min-h-screen">
       <Welcome />
       <Aboutus />
       <Footer />
+
+    </CardSpotlight>
+    </div>
+
     </>
   );
 }
