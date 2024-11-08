77a928fcc29a4592de188b9d306a11e5c39aace2
Mohd Atif Ansari <114794817+Atif468@users.noreply.github.com>
2024-11-08 17:16:07 +0530

    Update AuthContext.jsx
    small bug
    
    wrong:
    const [isLoggedIn, setIsLoggedIn] = useState("false");
    right:
    const [isLoggedIn, setIsLoggedIn] = useState(false);

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState, useEffect } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState("false");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
